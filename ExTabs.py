e68aedb6d7f6cc85fd5dfe1a87436f1b5bab0ca9
dreiksss <asemenow52@gmail.com>
2018-01-11 20:23:49 +0300

    Commit

--- ExTabs.py
+++ ExTabs.py
@@ -1,2 +1,16 @@
-def ExTabs(text, expand = true):
-	pass
+def ExTabs(text, isfile = False):
+	if isfile:
+		file = open(text, "r")
+		text = file.read()
+		file.close()
+	res = ""
+	index1 = 0
+	index2 = text.find("\t")
+	while index2 != -1:
+		res += text[index1:index2]
+		res += "    "
+		index1 = index2 + 1
+		index2 = text.find("\t", index1)
+	res += text[index1:]
+	return res
+
