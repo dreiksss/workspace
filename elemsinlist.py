7278781e04d71405917f6794ef1dfedc1ccd25c3
dreiksss <asemenow52@gmail.com>
2018-01-17 15:35:18 +0300

    commit

--- elemsinlist.py
+++ elemsinlist.py
@@ -1,1 +1,24 @@
+testList = [1, 2, 3, 4, 5, 1, 2, 3, 4, 1, 2, 3, 1, 2]
 
+def elems_in_list(mylist):
+	"""elems_in_list(list)
+
+	 -- return new list with unique elements of list in order from rarer to often
+
+	"""
+
+	res = []
+	dictSort = {}
+	for item in mylist:
+		occur = mylist.count(item)
+		if occur in dictSort:
+			if item not in dictSort[occur]:
+				dictSort[occur].append(item)
+		else:
+			dictSort[occur] = [item]
+	occurList = list(dictSort.keys())
+	occurList.sort()
+	for occur in occurList:
+		for num in dictSort[occur]:
+			res.append(num)
+	return res
