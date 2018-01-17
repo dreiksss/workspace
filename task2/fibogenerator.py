d5536f511e601482fb070f775e5c633a959e08e8
dreiksss <asemenow52@gmail.com>
2018-01-17 18:17:17 +0300

    commit

--- task2/fibogenerator.py
+++ task2/fibogenerator.py
@@ -1,1 +1,11 @@
+def fibo_generator(n):
+	"""fibo_generator(n) -- return generator for first n fibonachi numbers"""
 
+	a = 1
+	b = 1
+	for i in range(n):
+		print("before yield")
+		yield a
+		print("after yield")
+		a, b = b, a + b
+		print("end code")
