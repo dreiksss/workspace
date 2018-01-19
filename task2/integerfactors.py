12383e9da6a858470cf18ebbf95ffc93fa0ac4a6
dreiksss <asemenow52@gmail.com>
2018-01-19 15:49:49 +0300

    commit

--- task2/integerfactors.py
+++ task2/integerfactors.py
@@ -1,1 +1,21 @@
+def int_sums(number):
+	"""int_sums(number) -- return list of all integer sums for number
+	number = a + b + c
 
+	"""
+
+	if number == 1:
+		return [[1]]
+	res = []
+	for i in range(1, number):
+		for sublist in int_sums(i):
+			isSkip = False
+			for item in sublist:
+				if item > number-i:
+					isSkip = True
+					break
+			if isSkip:
+				continue
+			res.append([number-i])
+			res[-1].extend(sublist)
+	return res
